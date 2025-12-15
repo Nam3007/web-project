03b71d75df5db370678a80f2f539e025d1112355
Nam307 <145528937+Nam3007@users.noreply.github.com>
2025-12-15 12:58:41 +0700

    Delete web-project-master/BE/app directory

--- web-project-master/BE/app/controller/__init__.py
+++
@@ -1,5 +0,0 @@
-from .customer import router as customer_router
-from .staff import router as staff_router
-from .menu_item import router as menu_item_router
-from .order_item import router as order_item_router
-from .order import router as order_router
